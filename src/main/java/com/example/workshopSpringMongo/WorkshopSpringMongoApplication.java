38a9ec4ab6e7f2e2509ea6f872623ec11d2b4ea5
Joao Marcelo <joao.mm.santos@outlook.com>
2024-03-07 10:34:14 -0300

    first commit

---
+++ src/main/java/com/example/workshopSpringMongo/WorkshopSpringMongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.workshopSpringMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/workshopSpringMongo/WorkshopSpringMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workshopSpringMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

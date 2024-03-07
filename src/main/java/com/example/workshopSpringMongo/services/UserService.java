4a0de542df527fdd482594ac7d63b647115feab1
Joao Marcelo <joao.mm.santos@outlook.com>
2024-03-07 15:53:34 -0300

    findById and ObjectNotFoundException

--- src/main/java/com/example/workshopSpringMongo/services/UserService.java
+++ src/main/java/com/example/workshopSpringMongo/services/UserService.java
@@ -1,21 +1,28 @@
 package com.example.workshopSpringMongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.workshopSpringMongo.domain.User;
 import com.example.workshopSpringMongo.repository.UserRepository;
+import com.example.workshopSpringMongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
 }
